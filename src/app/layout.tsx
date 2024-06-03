6990a4db41c7c7b4b60a1e0b859986b5191ffa14
Christian Stamati <christian.stamati@gmail.com>
2024-06-04 00:21:41 +0200

    modify metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
+import "../styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../styles/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
-  title: "Turbo Ship",
-  description: "The fastest way to start a next project",
+  title: "Threedify",
+  description: "One platform infinite configurations",
   icons: [{ rel: "icon", url: "/favicon.png" }],
 };
 
