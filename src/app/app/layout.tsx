6bf14534d7b31b0579d67d2bc6d351f3ad6c4967
Christian Stamati <christian.stamati@gmail.com>
2024-06-05 00:36:49 +0200

    fix page height

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="grid min-h-svh w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <SideNavigation />
       <div className="flex flex-col">
         <Header />
