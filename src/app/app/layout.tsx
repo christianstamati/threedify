9b75dec17adfb60db1808e2bbb68dff1001111d7
Christian Stamati <christian.stamati@gmail.com>
2024-06-06 22:40:58 +0200

    removed old dashboard

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,18 +1,7 @@
-import Header from "@/components/header";
-import SideNavigation from "@/components/side-navigation";
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <div className="grid min-h-svh w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <SideNavigation />
-      <div className="flex flex-col">
-        <Header />
-        {children}
-      </div>
-    </div>
-  );
+  return <div className=""></div>;
 }
